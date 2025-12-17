ba5cc4ce6d0925d9888e764d0ec8c2f89d2dc5f7
Marco2a94 <marco.arrighi@efrei.net>
2025-12-17 11:55:23 +0100

    Refactor MongoDataset class for improved readability and structure

--- lxp-ia/src/lxp_ia/datasets/mongo_dataset.py
+++ lxp-ia/src/lxp_ia/datasets/mongo_dataset.py
@@ -2,6 +2,7 @@ from kedro.io import AbstractDataset
 from pymongo import MongoClient
 import pandas as pd
 
+
 class MongoDataset(AbstractDataset):
     def __init__(self, uri, db, collection):
         self.uri = uri
@@ -10,8 +11,17 @@ class MongoDataset(AbstractDataset):
 
     def _load(self):
         client = MongoClient(self.uri)
-        data = list(client[self.db][self.collection].find({}, {"_id": 0}))
+        data = list(
+            client[self.db][self.collection].find({}, {"_id": 0})
+        )
         return pd.DataFrame(data)
 
     def _save(self, data):
         raise NotImplementedError("Read-only dataset")
+
+    def _describe(self):
+        return {
+            "uri": self.uri,
+            "db": self.db,
+            "collection": self.collection,
+        }
