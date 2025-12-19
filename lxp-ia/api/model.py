389de55760b50f0998cb566ec3be15e9f80faad4
Marco2a94 <102526094+Marco2a94@users.noreply.github.com>
2025-12-19 01:26:37 +0100

    Implement fallback mechanism in model loading function to ensure model retrieval from the latest version if the production model fails to load. This enhances robustness in model deployment.

--- lxp-ia/api/model.py
+++ lxp-ia/api/model.py
@@ -4,6 +4,10 @@ import mlflow.sklearn
 MODEL_NAME = "bike_forecast_multi_horizon"
 
 def load_model():
-    model_uri = f"models:/{MODEL_NAME}/Production"
-    model = mlflow.sklearn.load_model(model_uri)
-    return model
+    try:
+        model_uri = f"models:/{MODEL_NAME}/Production"
+        return mlflow.sklearn.load_model(model_uri)
+    except Exception:
+        # fallback : dernière version disponible
+        model_uri = f"models:/{MODEL_NAME}/latest"
+        return mlflow.sklearn.load_model(model_uri)
